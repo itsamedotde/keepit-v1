1c4da56a68104c69963ced6c1172ad75ab19927c
MarioT <dev@mario.cx>
2020-12-22 18:27:37 +0100

    Fix bug in preload

--- backend/src/Controller/Api/KeepitApiController.php
+++ backend/src/Controller/Api/KeepitApiController.php
@@ -37,8 +37,8 @@ class KeepitApiController extends AbstractController
         ImageRepository $imageRepository, 
         TagRepository $tagRepository, 
         UserRepository $userRepository, 
-        KeepitRepository $keepitRepository,
-        GeoService $locationService
+        KeepitRepository $keepitRepository
+        //GeoService $locationService
         ) {
 
         $requestContent = json_decode($request->getContent(), true); 
