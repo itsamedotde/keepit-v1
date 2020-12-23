fef40e1cab3fc663eebe32b02dfae4c8ef211ad3
MarioT <dev@mario.cx>
2020-12-23 13:59:46 +0100

    Fix bug in preload

--- backend/src/Controller/Api/KeepitApiController.php
+++ backend/src/Controller/Api/KeepitApiController.php
@@ -62,35 +62,35 @@ class KeepitApiController extends AbstractController
             $response = new JsonResponse($longitude);
             
       
-            //$locationData = $locationService->getLocationData($latitude, $longitude);
+            $locationData = $locationService->getLocationData($latitude, $longitude);
             // $response = new JsonResponse($test);
             // return $response;
             // die;
+            error_log("Your message here");
+            // $url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='.$latitude.','.$longitude.'&sensor=false&key='.$_ENV['MAP_API_KEY'];
+            // $json = @file_get_contents($url);
+            // $data = json_decode($json);
+            // $status = $data->status;
+            // if($status=="OK") {
+            //     for ($j=0;$j<count($data->results[0]->address_components);$j++) {
+            //         $cn=array($data->results[0]->address_components[$j]->types[0]);
+            //         if(in_array("locality", $cn)) {
+            //             $city= $data->results[0]->address_components[$j]->long_name;
+            //         }
+            //         if(in_array("country", $cn)) {
+            //             $country= $data->results[0]->address_components[$j]->long_name;
+            //         }
+            //     }
+            //     //return [$city , $country];
+            //  } else{
+            //    $city = 'Unknown';
+            //    $country = 'Unknown';
+            //    //return [$city , $country];
+            //  }
 
-            $url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='.$latitude.','.$longitude.'&sensor=false&key='.$_ENV['MAP_API_KEY'];
-            $json = @file_get_contents($url);
-            $data = json_decode($json);
-            $status = $data->status;
-            if($status=="OK") {
-                for ($j=0;$j<count($data->results[0]->address_components);$j++) {
-                    $cn=array($data->results[0]->address_components[$j]->types[0]);
-                    if(in_array("locality", $cn)) {
-                        $city= $data->results[0]->address_components[$j]->long_name;
-                    }
-                    if(in_array("country", $cn)) {
-                        $country= $data->results[0]->address_components[$j]->long_name;
-                    }
-                }
-                //return [$city , $country];
-             } else{
-               $city = 'Unknown';
-               $country = 'Unknown';
-               //return [$city , $country];
-             }
 
-
-            // $city = $locationData[0];
-            // $country = $locationData[1];
+            $city = $locationData[0];
+            $country = $locationData[1];
             $newKeepit->setCity($city);
             $newKeepit->setCountry($country);
             $newKeepit->setLatitude($latitude);
