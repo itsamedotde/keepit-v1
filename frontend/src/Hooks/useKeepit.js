1577bb46b833b0adbb621bb789ff221fff8da4ad
MarioT <dev@mario.cx>
2021-01-04 09:47:22 +0100

    Reworked timing for adding own tags

--- frontend/src/Hooks/useKeepit.js
+++ frontend/src/Hooks/useKeepit.js
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import { apiDeleteKeepit, apiSaveKeepit } from '../Services/apiRequests.js'
+import { apiGetAllKeepits } from '../Services/apiRequests.js'
 
 export default function useKeepit() {
   //const [geolocation, setGeolocation] = useState([])
+  const [keepits, setKeepits] = useState([])
 
-  return { deleteKeepit, saveKeepit }
+  return { keepits, setKeepits, deleteKeepit, saveKeepit, loadKeepitsFromApi }
 
   function deleteKeepit(id) {
     apiDeleteKeepit(id)
@@ -15,6 +17,18 @@ export default function useKeepit() {
   function saveKeepit(request, handleApiTags) {
     apiSaveKeepit(request)
       .then((result) => console.log(result))
+      .catch((error) => console.log('error', error))
+  }
+
+  function loadKeepitsFromApi() {
+    apiGetAllKeepits()
+      .then((result) =>
+        setKeepits(
+          result.sort(function (a, b) {
+            return b.id - a.id
+          })
+        )
+      )
       .catch((error) => console.log('error', error))
   }
 }
