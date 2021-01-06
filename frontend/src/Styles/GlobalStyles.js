9d939b000f5e23d2b601914ad13da897be5c883a
MarioT <dev@mario.cx>
2021-01-06 11:40:33 +0100

    Design fixes

--- frontend/src/Styles/GlobalStyles.js
+++ frontend/src/Styles/GlobalStyles.js
@@ -86,4 +86,13 @@ textarea {
   font-size: 16px;
 }
 
+* {
+    -webkit-touch-callout:none;
+    -webkit-user-select:none;
+    -moz-user-select:none;
+    -ms-user-select:none;
+    user-select:none;
+}
+
+
 `
